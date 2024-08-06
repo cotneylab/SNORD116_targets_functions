84cf886082006adadf08480c83538ce7e5b4f4a4
Rachel Gilmore <rgilmore@uchc.edu>
2024-08-06 10:31:24 -0400

    Delete Bulk RNA-Seq Analysis/Step01_ConcatenateFiles/lgDEL/1-2_make_launch_script_fastqc_rbg.sh
    Uploaded incorrect file.

--- Bulk RNA-Seq Analysis/Step01_ConcatenateFiles/lgDEL/1-2_make_launch_script_fastqc_rbg.sh
+++
@@ -1,8 +0,0 @@
-#Go to folder with slurm output scripts from make_highthroughput_fastqc_scripts.sh
-cd /home/FCAM/rgilmore/data/rna-seq/NGNneurons/PWS3/slurm
-#Make script to launch files
-ls *fastqc*.slurm | awk '{print "sbatch "$1}' > launch_fastqc.sh
-#Make script executeable
-chmod +x launch_fastqc.sh
-#LAUNCH SCRIPT!!
-./launch_fastqc.sh
